e61d0367b6730d52c5e7529036daffe06e41e927
Daniil Novoselov <gudnmail@gmail.com>
2021-08-04 23:51:55 +0500

    feat: storybook dark theme

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-svelte-csf',
+    '@storybook/addon-storysource',
   ],
   svelteOptions: {
     preprocess: require('svelte-preprocess')(),
